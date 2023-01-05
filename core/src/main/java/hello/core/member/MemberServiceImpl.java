f0bf7feb831bc15f248042a1fc3c5955b21f2f11
ljh468 <ljh468@naver.com>
2023-01-05 20:37:13 +0900

    DI 적용

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-  private final MemberRepository memberRepository = new MemoryMemberRepository();
+  // 인터페이스만 의존하게 됨.
+  private final MemberRepository memberRepository;
+
+  // 생성자 주입
+  public MemberServiceImpl(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
 
   @Override
   public void join(Member member) {
