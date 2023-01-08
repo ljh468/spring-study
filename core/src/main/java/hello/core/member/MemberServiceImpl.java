2271ab83662c3ec2f1f08c064a66f9d3d760c9b2
ljh468 <ljh468@naver.com>
2023-01-09 00:13:55 +0900

    @ComponentScan

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,17 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
   // 인터페이스만 의존하게 됨.
   private final MemberRepository memberRepository;
 
   // 생성자 주입
+  // 의존관계 자동주입
+  @Autowired
   public MemberServiceImpl(MemberRepository memberRepository) {
     this.memberRepository = memberRepository;
   }
