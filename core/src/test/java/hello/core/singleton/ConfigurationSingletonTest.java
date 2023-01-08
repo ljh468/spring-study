c465bd2c9817ed2f405ec939ccb02f770dc6ce09
ljh468 <ljh468@naver.com>
2023-01-08 20:42:40 +0900

    @Configuration과 바이트코드 조작의 마법

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -8,6 +8,7 @@ import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,5 +33,13 @@ public class ConfigurationSingletonTest {
     assertThat(memberRepository2).isSameAs(memberRepository);
 
     // memberRepository 인스턴스는 모두 같은 인스턴스가 공유되어 사용된다.
+  }
+
+  @Test
+  void configurationDeep() {
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    AppConfig bean = applicationContext.getBean(AppConfig.class);
+    System.out.println("bean = " + bean.getClass());
+    // bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$
   }
 }
