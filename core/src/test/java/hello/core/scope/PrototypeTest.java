5ccfb21bbc0d85a793e3f71ad2cc142482378b3d
ljh468 <ljh468@naver.com>
2023-01-10 22:29:45 +0900

    빈스코프

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,45 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+  @Test
+  void prototypeBeanFind() {
+    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(PrototypeBean.class);
+    System.out.println("find prototypeBean1");
+    PrototypeBean prototypeBean1 = applicationContext.getBean(PrototypeBean.class);
+
+    System.out.println("find prototypeBean2");
+    PrototypeBean prototypeBean2 = applicationContext.getBean(PrototypeBean.class);
+    System.out.println("prototypeBean1 = " + prototypeBean1);
+    System.out.println("prototypeBean2 = " + prototypeBean2);
+
+    assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+    applicationContext.close();
+
+    // destroy가 안되기 때문에 수동으로 호출해야 한다.
+    prototypeBean1.destroy();
+    prototypeBean2.destroy();
+  }
+
+  @Scope("prototype")
+  static class PrototypeBean {
+    @PostConstruct
+    public void init() {
+      System.out.println("PrototypeBean.init");
+    }
+
+    @PreDestroy
+    public void destroy() {
+      System.out.println("PrototypeBean.destroy");
+    }
+  }
+}
