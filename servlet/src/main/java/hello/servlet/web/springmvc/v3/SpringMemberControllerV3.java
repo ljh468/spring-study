d80fdbe039ba241c4ceabe549af65eb3c7e1e4f9
ljh468 <ljh468@naver.com>
2023-01-27 06:56:22 +0900

    getMapping postMapping

--- servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,6 +4,8 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,12 +27,12 @@ public class SpringMemberControllerV3 {
 
   private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-  @RequestMapping("/new-form")
+  @GetMapping("/new-form")
   public String newForm() {
     return "new-form";
   }
 
-  @RequestMapping("/save")
+  @PostMapping("/save")
   public String save(
       @RequestParam("username") String username,
       @RequestParam("age") int age,
@@ -48,7 +50,7 @@ public class SpringMemberControllerV3 {
   }
 
   // /springmvc/v3/members
-  @RequestMapping
+  @GetMapping
   public String members(Model model) {
 
     List<Member> members = memberRepository.findAll();
