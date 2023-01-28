94d0868d50b0cd68ad48628229a71eb8cf406e48
ljh468 <ljh468@naver.com>
2023-01-28 13:32:30 +0900

    item domain generate

---
+++ item-service/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -0,0 +1,39 @@
+package hello.itemservice.domain.item.basic;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+  private final ItemRepository itemRepository;
+
+  @GetMapping
+  public String items(Model model) {
+    List<Item> items = itemRepository.findAll();
+    log.info("items={}", items);
+    model.addAttribute("items", items);
+    return "basic/items";
+  }
+
+  /**
+   * 테스트용 데이터 추가
+   */
+  @PostConstruct
+  public void init() {
+    itemRepository.save(new Item("testA", 10000, 10));
+    itemRepository.save(new Item("testB", 20000, 20));
+  }
+}
