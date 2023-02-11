78b4ffc8259448d3c2ad547581fc3fa37d2caa8f
ljh468 <ljh468@naver.com>
2023-02-11 22:53:59 +0900

    beanValidation 한계

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+// @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합을 10000원 넘게 입력하세요.")
 public class Item {
 
 //    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
