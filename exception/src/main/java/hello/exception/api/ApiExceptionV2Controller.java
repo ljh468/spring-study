02a38d6cfd6359dd95a42daa4a54ddc0eca7aa33
ljh468 <ljh468@naver.com>
2023-02-22 04:10:25 +0900

    RestControllerAdvice

--- exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -13,20 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ApiExceptionV2Controller {
 
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  @ExceptionHandler(IllegalArgumentException.class)
-  public ErrorResult illegalExHandler(IllegalArgumentException e) {
-    log.error("[exceptionHandler] ex", e);
-    return new ErrorResult("BAD", e.getMessage());
-  }
-
-  @ExceptionHandler
-  public ResponseEntity<ErrorResult> userExHandle(UserException e) {
-    log.error("[exceptionHandle] ex", e);
-    ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-    return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-  }
-
   @GetMapping("/api2/members/{id}")
   public ApiExceptionController.MemberDto getMember(@PathVariable String id) {
     if (id.equals("ex")) {
