78261049f4a13a913ede77cb76fd9ef53192d26b
ljh468 <ljh468@naver.com>
2023-02-26 20:18:33 +0900

    @Transactional AOP

--- db1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ db1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -1,22 +1,32 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV1;
+import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.SQLException;
 
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
 @Slf4j
 @RequiredArgsConstructor
-public class MemberServiceV1 {
+public class MemberServiceV3_3 {
 
-  private final MemberRepositoryV1 memberRepository;;
+  private final MemberRepositoryV3 memberRepository;
 
+  @Transactional
   public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+    bizLogic(fromId, toId, money);
+  }
+
+  private void bizLogic(String fromId, String toId, int money) throws SQLException {
     Member fromMember = memberRepository.findById(fromId);
     Member toMember = memberRepository.findById(toId);
-
     memberRepository.update(fromId, fromMember.getMoney() - money);
     validation(toMember);
     memberRepository.update(toId, toMember.getMoney() + money);
@@ -27,4 +37,5 @@ public class MemberServiceV1 {
       throw new IllegalStateException("이체 중 예외발생");
     }
   }
+
 }
